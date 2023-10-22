b207f441cccc778448775ae119f594058f058b3a
Mag. A <118616073+magamudi@users.noreply.github.com>
2023-10-22 18:38:07 -0500

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.5.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.6.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+  #   access_key = var.aws_access_key
+  #   secret_key = var.aws_secret_key
+}
